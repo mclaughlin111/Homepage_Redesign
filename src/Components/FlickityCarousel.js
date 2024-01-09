d654994c3c7e8295c78d957361b01f9e0409caca
Thomas McLaughlin <tmclaughlin5.1111@gmail.com>
2024-01-09 10:29:51 +0000

    added description to carousel images, added transition to dark/light switch

--- src/Components/FlickityCarousel.js
+++ src/Components/FlickityCarousel.js
@@ -3,14 +3,16 @@ import React from "react";
 import Flickity from "react-flickity-component";
 import pentaxImage from "../Assets/pentaxcamera.jpeg";
 import youtubeImage from "../Assets/youtubelink.jpeg";
-import gameImage from "../Assets/gamelink.jpeg";
+import gameImage from "../Assets/gamelink.png";
+
 import "../flickity.css"
 
 const flickityOptions = {
   initialIndex: 0,
   pageDots: true,
   selectedAttraction: 0.1,
-friction: 1
+friction: 1,
+draggable: false,
 };
 
 const FlickityCarousel = () => {
@@ -20,9 +22,12 @@ const FlickityCarousel = () => {
     <Flickity options={flickityOptions}>
   
 
-<a href="/photosfilm"><img  src={pentaxImage} alt="pentaxImage" className="carousel-image"/></a>
-<a href="https://www.youtube.com/@ThomasMYoutube"><img src={youtubeImage} alt="youtubeImage" className="carousel-image"/></a>
-<a href="/game"><img src={gameImage} alt="gameImage" className="carousel-image" id="gameImage"/></a>
+<div className="carouselItemContainer"><a href="/photosfilm"><img  src={pentaxImage} alt="pentaxImage" className="carousel-image"/></a><p className="carouselItemDescription">Photography Gallery</p></div>
+
+<div className="carouselItemContainer"><a href="https://www.youtube.com/@ThomasMYoutube"><img src={youtubeImage} alt="youtubeImage" className="carousel-image"/></a><p className="carouselItemDescription">Video Portfolio</p></div>
+
+
+<div className="carouselItemContainer"><a href="/game"><img src={gameImage} alt="gameImage" className="carousel-image" id="gameImage"/></a><p className="carouselItemDescription">Memory Training Game</p></div>
   </Flickity>
 
   </div>
