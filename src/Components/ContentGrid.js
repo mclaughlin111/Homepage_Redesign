0f8cb8365392233908f701e8b767005c6b1713c0
Thomas McLaughlin <tmclaughlin5.1111@gmail.com>
2024-02-11 10:32:55 +0000

    added project list view

--- src/Components/ContentGrid.js
+++ src/Components/ContentGrid.js
@@ -1,16 +1,44 @@
 import React from "react";
+import pentaxImage from "../Assets/pentaxcamera.jpeg";
+import youtubeImage from "../Assets/youtubelink.jpeg";
+import gameImage from "../Assets/gamelink.png";
+import pitchKick from "../Assets/pitchkick.jpeg";
+import "../contentgrid.css";
 
 const ContentGrid = () => {
-  return( 
-  <table>
-  <tr>
-    <td>Photos</td>
-    </tr>
-    <tr><td>Pitch Kick</td></tr>
-    <tr><td>Game</td></tr>
-    <tr><td>Youtube</td></tr>
-  </table>
-  )
+  return (
+    <table className="table">
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Last Updated</th>
+      </tr>
+      <tr id="topRow">
+        <td>
+          <img src={pentaxImage} alt="pentaxImage" className="tableImage" />
+        </td>
+        <td>Photos</td>
+        <td className="tableDescription">Image gallery to showcase personal photography, built around <a href="https://www.lightgalleryjs.com/">lightgallery.js</a> plugin</td>
+        
+      </tr>
+      <tr>
+        <td><img src={pitchKick} alt="pitchkick" className="tableImage" /></td>
+        <td>Pitch Kick</td>
+        <td className="tableDescription">A sandbox Analog style kick drum synth built to test out the  <a href="https://tonejs.github.io/">tone.js</a> framework</td>
+      </tr>
+      <tr>
+        <td><img src={gameImage} alt="gameImage" className="tableImage" /></td>
+        <td>Memory Game</td>
+        <td className="tableDescription">Interactive level-based puzzle game to test and develop memory recall.</td>
+      </tr>
+      <tr>
+        <td><img src={youtubeImage} alt="youtubeImage" className="tableImage" /></td>
+        <td>Youtube</td>
+        <td className="tableDescription">Youtube channel with videos relating to music / audio / sound-engineering</td>
+      </tr>
+    </table>
+  );
 };
 
 export default ContentGrid;
