73a6960df582c597623b3f1d2d6caacd81c2f1d3
Thomas McLaughlin <tmclaughlin5.1111@gmail.com>
2024-02-15 15:29:17 +0000

    small changes re links and styling

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,6 +4,7 @@ import FlickityCarousel from "../Components/FlickityCarousel";
 import { TbCarouselHorizontal } from "react-icons/tb";
 import { PiListFill } from "react-icons/pi";
 import { IoGrid } from "react-icons/io5";
+import { BsFillGridFill } from "react-icons/bs";
 import "../projects.css";
 import ContentGrid from "./ContentGrid";
 
@@ -17,31 +18,41 @@ const Projects = () => {
   return (
     <div>
       <nav>
-        <ul>
-          <li
-            className={activeComponent === "contentList" ? "active" : ""}
-            onClick={() => handleToggleComponent("contentList")}
-          >
-            <PiListFill />
-          </li>
-          <li
-            className={activeComponent === "flickityCarousel" ? "active" : ""}
-            onClick={() => handleToggleComponent("flickityCarousel")}
-          >
-            <TbCarouselHorizontal />
-          </li>
-          <li
-            className={activeComponent === "contentGrid" ? "active" : ""}
-            onClick={() => handleToggleComponent("contentGrid")}
-          >
-            <IoGrid />
-          </li>
-        </ul>
+      <ul>
+  <div className={activeComponent === "contentList" ? "active" : ""}>
+    <li
+      className={activeComponent === "contentList" ? "active" : ""}
+      onClick={() => handleToggleComponent("contentList")}
+    >
+      <PiListFill />
+    </li>
+  </div>
+
+  <div className={activeComponent === "flickityCarousel" ? "active" : ""}>
+    <li
+      className={activeComponent === "flickityCarousel" ? "active" : ""}
+      onClick={() => handleToggleComponent("flickityCarousel")}
+    >
+      <TbCarouselHorizontal />
+    </li>
+  </div>
+
+  <div className={activeComponent === "contentGrid" ? "active" : ""}>
+    <li
+      className={activeComponent === "contentGrid" ? "active" : ""}
+      onClick={() => handleToggleComponent("contentGrid")}
+    >
+      <BsFillGridFill />
+    </li>
+  </div>
+</ul>    
+        
         <div
           className={`selector ${
             activeComponent === "flickityCarousel" ? "right" : ""
           }`}
         />
+   
       </nav>
 
       {activeComponent === "contentList" && <ContentList />}
